fbf25cb111ef94ff2ad561cb09e0cf1ed4872188
Kirill Kovalev <k.s.kovalev@gmail.com>
2024-12-13 18:33:16 +0300

    Test_SelectClient_WhenOk is done

--- main_test.go
+++ main_test.go
@@ -1,25 +1,48 @@
 package main
 
 import (
+	"database/sql"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	_ "modernc.org/sqlite"
 )
 
 func Test_SelectClient_WhenOk(t *testing.T) {
-	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")
+	if err != nil {
+		require.NoError(t, err)
+	}
+	defer db.Close()
 
 	clientID := 1
 
-	// напиши тест здесь
+	client, err := selectClient(db, clientID)
+	if err != nil {
+		require.NoError(t, err)
+	}
+	assert.Equal(t, client.ID, clientID)
+	assert.NotEmpty(t, client.FIO)
+	assert.NotEmpty(t, client.Login)
+	assert.NotEmpty(t, client.Birthday)
+	assert.NotEmpty(t, client.Email)
 }
 
 func Test_SelectClient_WhenNoClient(t *testing.T) {
-	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")
+	if err != nil {
+		require.NoError(t, err)
+	}
+	defer db.Close()
 
 	clientID := -1
 
-	// напиши тест здесь
+	client, err := selectClient(db, clientID)
+	if err != nil {
+		require.EqualError(t, err, "sql.ErrNoRows")
+	}
 }
 
 func Test_InsertClient_ThenSelectAndCheck(t *testing.T) {
